96077198fb43d3a538ded4379c9f434d581ba611
おバカな槿槿殿 <lmjzhizun@gmail.com>
2021-03-19 09:30:47 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,6 +15,3 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-
-# Add vernesong/OpenClash
-sed -i '$a src-git openclash https://github.com/vernesong/OpenClash;dev' feeds.conf.default
